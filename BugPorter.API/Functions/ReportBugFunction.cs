817c679121287525ff988ce2085522044ff50a87
André <andredekamacedo@gmail.com>
2023-08-04 20:14:12 -0300

    #107 - Add Authentication

--- BugPorter.API/Functions/ReportBugFunction.cs
+++ BugPorter.API/Functions/ReportBugFunction.cs
@@ -1,32 +1,48 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using BugPorter.API.Features.ReportBug.GitHub;
 using BugPorter.API.Features.ReportBug;
+using FirebaseAdminAuthentication.DependencyInjection.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using FirebaseAdminAuthentication.DependencyInjection.Models;
 
 namespace BugPorter.API
 {
     public  class ReportBugFunction
     {
         readonly CreateGitHubIssueCommand _createGitHubIssueCommand;
+        readonly FirebaseAuthenticationFunctionHandler _authenticationHandler;
         readonly ILogger<ReportBugFunction> _logger;
 
-        public ReportBugFunction(ILogger<ReportBugFunction> logger, CreateGitHubIssueCommand createGitHubIssueCommand)
+        public ReportBugFunction(
+            ILogger<ReportBugFunction> logger, 
+            CreateGitHubIssueCommand createGitHubIssueCommand, 
+            FirebaseAuthenticationFunctionHandler authenticationHandler)
         {
             _logger = logger;
             _createGitHubIssueCommand = createGitHubIssueCommand;
+            _authenticationHandler = authenticationHandler;
         }
 
         [FunctionName("ReportBugFunction")]
         public  async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "bugs")] ReportedBugRequest request)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "bugs")] ReportedBugRequest request, 
+            HttpRequest httpRequest)
         {
+            AuthenticateResult authenticateResult = await _authenticationHandler.HandleAuthenticateAsync(httpRequest);
+
+            if (!authenticateResult.Succeeded)
+            {
+                return new UnauthorizedResult();
+            }
+
+            string userId = authenticateResult.Principal.FindFirst(FirebaseUserClaimType.ID).Value;
+            _logger.LogInformation("Authenticated user {UserId}", userId);
+
             NewBug newBug = new NewBug(request.Summary, request.Description);
 
             ReportedBug reportedBug = await _createGitHubIssueCommand.Execute(newBug);
--- BugPorter.API/Startup.cs
+++ BugPorter.API/Startup.cs
@@ -1,14 +1,11 @@
 ﻿using BugPorter.API.Features.ReportBug.GitHub;
+using FirebaseAdmin;
+using FirebaseAdminAuthentication.DependencyInjection.Extensions;
+using Google.Apis.Auth.OAuth2;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Octokit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 [assembly: FunctionsStartup(typeof(BugPorter.API.Startup))]
 namespace BugPorter.API
 {
@@ -17,8 +14,15 @@ namespace BugPorter.API
         public override void Configure(IFunctionsHostBuilder builder)
         {
             IConfiguration configuration = builder.GetContext().Configuration;
+            string firebaseConfig = configuration.GetValue<string>("FIREBASE_CONFIG");
 
-
+            FirebaseApp firebaseApp = FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromJson(firebaseConfig)
+            });
+            builder.Services.AddSingleton(firebaseApp);
+            builder.Services.AddFirebaseAuthentication();
+            
             builder.Services.Configure<GitHubRepositoryOptions>(options =>
             {
                 options.Owner = configuration.GetValue<string>("GITHUB_REPOSITORY_OWNER");
