5925ca40f64726f03aa0036dccd5fe3a9abf6017
André <andredekamacedo@gmail.com>
2023-08-05 17:29:19 -0300

    #108 - Organize Startup class

--- BugPorter.API/Startup.cs
+++ BugPorter.API/Startup.cs
@@ -5,6 +5,7 @@ using Google.Apis.Auth.OAuth2;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Octokit;
 using System.IO;
 
@@ -16,7 +17,6 @@ namespace BugPorter.API
         public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
         {
             FunctionsHostBuilderContext context = builder.GetContext();
-
             builder.ConfigurationBuilder
                 .AddJsonFile(Path.Combine(context.ApplicationRootPath, "host.json"), optional: true, reloadOnChange: false)
                 .AddJsonFile(Path.Combine(context.ApplicationRootPath, $"{context.EnvironmentName}.settings.json"), optional: true, reloadOnChange: false)
@@ -24,35 +24,21 @@ namespace BugPorter.API
         }
         public override void Configure(IFunctionsHostBuilder builder)
         {
-
             IConfiguration configuration = builder.GetContext().Configuration;
-            builder.Services.AddFirebaseAuthentication();
-            builder.Services.AddSingleton<GoogleCredential>();
-            builder.Services.AddSingleton<Credentials>();
-            builder.Services.AddSingleton<GitHubRepositoryOptions>();
 
-            builder.Services.Configure<GitHubRepositoryOptions>(options =>
-            {
-                options.Owner = configuration.GetValue<string>("GITHUB_REPOSITORY_OWNER");
-                options.Name = configuration.GetValue<string>("GITHUB_REPOSITORY_NAME");
-            });
+            string firebaseConfig = configuration.GetValue<string>("FIREBASE_CONFIG");            
             string gitHubToken = configuration.GetValue<string>("GITHUB_TOKEN");
-            builder.Services.AddSingleton(new GitHubClient(new ProductHeaderValue("bugporter-api"))
-            {
-                Credentials = new Credentials(gitHubToken)
-            });
-            builder.Services.AddSingleton<CreateGitHubIssueCommand>();
+            string owner = configuration.GetValue<string>("GITHUB_REPOSITORY_OWNER");
+            string name = configuration.GetValue<string>("GITHUB_REPOSITORY_NAME");
+            FirebaseApp firebaseApp = FirebaseApp.Create(new AppOptions(){Credential = GoogleCredential.FromJson(firebaseConfig).CreateScoped(new[] { "FIREBASE_CONFIG" })});
 
-            string firebaseConfig = configuration.GetValue<string>("FIREBASE_CONFIG");
-
-            var gCredential = GoogleCredential.FromJson(firebaseConfig).CreateScoped(new[] {"FIREBASE_CONFIG"});
-
-            FirebaseApp firebaseApp = FirebaseApp.Create(new AppOptions()
-            {
-                Credential = gCredential
-            });
             builder.Services.AddSingleton(firebaseApp);
-
+            builder.Services.AddSingleton<GoogleCredential>();
+            builder.Services.AddSingleton<Credentials>();            
+            builder.Services.AddSingleton<CreateGitHubIssueCommand>();
+            builder.Services.AddSingleton(new GitHubClient(new ProductHeaderValue("bugporter-api")){Credentials = new Credentials(gitHubToken)});
+            builder.Services.Configure<GitHubRepositoryOptions>(options =>{ options.Owner = owner; options.Name = name; });
+            builder.Services.AddFirebaseAuthentication();
         }
     }
 
