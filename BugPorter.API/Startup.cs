1f473cd5b664bfed320f1b7bfe582e6813ba018a
André <andredekamacedo@gmail.com>
2023-08-05 14:57:04 -0300

    #108 - Deployment azure functions

--- BugPorter.API/Startup.cs
+++ BugPorter.API/Startup.cs
@@ -6,23 +6,31 @@ using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Octokit;
+using System.IO;
+
 [assembly: FunctionsStartup(typeof(BugPorter.API.Startup))]
 namespace BugPorter.API
 {
     public class Startup : FunctionsStartup
     {
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            FunctionsHostBuilderContext context = builder.GetContext();
+
+            builder.ConfigurationBuilder
+                .AddJsonFile(Path.Combine(context.ApplicationRootPath, "host.json"), optional: true, reloadOnChange: false)
+                .AddJsonFile(Path.Combine(context.ApplicationRootPath, $"{context.EnvironmentName}.settings.json"), optional: true, reloadOnChange: false)
+                .AddEnvironmentVariables();
+        }
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            IConfiguration configuration = builder.GetContext().Configuration;
-            string firebaseConfig = configuration.GetValue<string>("FIREBASE_CONFIG");
 
-            FirebaseApp firebaseApp = FirebaseApp.Create(new AppOptions()
-            {
-                Credential = GoogleCredential.FromJson(firebaseConfig)
-            });
-            builder.Services.AddSingleton(firebaseApp);
+            IConfiguration configuration = builder.GetContext().Configuration;
             builder.Services.AddFirebaseAuthentication();
-            
+            builder.Services.AddSingleton<GoogleCredential>();
+            builder.Services.AddSingleton<Credentials>();
+            builder.Services.AddSingleton<GitHubRepositoryOptions>();
+
             builder.Services.Configure<GitHubRepositoryOptions>(options =>
             {
                 options.Owner = configuration.GetValue<string>("GITHUB_REPOSITORY_OWNER");
@@ -34,7 +42,18 @@ namespace BugPorter.API
                 Credentials = new Credentials(gitHubToken)
             });
             builder.Services.AddSingleton<CreateGitHubIssueCommand>();
+
+            string firebaseConfig = configuration.GetValue<string>("FIREBASE_CONFIG");
+
+            var gCredential = GoogleCredential.FromJson(firebaseConfig).CreateScoped(new[] {"FIREBASE_CONFIG"});
+
+            FirebaseApp firebaseApp = FirebaseApp.Create(new AppOptions()
+            {
+                Credential = gCredential
+            });
+            builder.Services.AddSingleton(firebaseApp);
+
         }
     }
 
-} 
+}
