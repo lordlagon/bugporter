2659d0c3ba86dcc75976db900d00933d737833e1
André <andredekamacedo@gmail.com>
2023-08-05 17:36:54 -0300

    #112 - Create MAUI client project

---
+++ BugPorter.Client/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BugPorter.Client
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ BugPorter.Client/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BugPorter.Client
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
