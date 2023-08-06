cd95fd3878b9fed5ef54259ec5a3b4539c109420
André <andredekamacedo@gmail.com>
2023-08-05 21:20:53 -0300

    #114 Create report bug UI

---
+++ BugPorter.Client/Features/ReportBug/ReportBugFormView.xaml.cs
@@ -0,0 +1,9 @@
+namespace BugPorter.Client;
+
+public partial class ReportBugFormView : ContentView
+{
+	public ReportBugFormView()
+	{
+		InitializeComponent();
+	}
+}
---
+++ BugPorter.Client/Features/ReportBug/ReportBugFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace BugPorter.Client;
+
+public partial class ReportBugFormViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    string summary;
+
+    [ObservableProperty]
+    string description;
+
+    [RelayCommand]
+    public void ReportBug()
+    {
+
+    }
+}
