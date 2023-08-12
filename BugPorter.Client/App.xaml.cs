8d2d6ad696ea0dee7a93c5b90ff388af00dd159c
André <andredekamacedo@gmail.com>
2023-08-12 19:38:22 -0300

    #122 - Navigate to sign in page on start up

--- BugPorter.Client/App.xaml.cs
+++ BugPorter.Client/App.xaml.cs
@@ -5,7 +5,12 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-
 		MainPage = new AppShell();
 	}
+    protected override async void OnStart()
+    {
+		await Shell.Current.GoToAsync("//SignIn");
+
+        base.OnStart();
+    }
 }
--- BugPorter.Client/AppShell.xaml.cs
+++ BugPorter.Client/AppShell.xaml.cs
@@ -5,5 +5,8 @@ public partial class AppShell : Shell
 	public AppShell()
 	{
 		InitializeComponent();
-	}
+		Routing.RegisterRoute(nameof(SignUpView), typeof(SignUpView));
+        Routing.RegisterRoute(nameof(SignInView), typeof(SignInView));
+        Routing.RegisterRoute(nameof(ReportBugView), typeof(ReportBugView));
+    }
 }
