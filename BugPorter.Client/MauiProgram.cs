faa5562d6427ea77eec2e9b683bd714cb5a05c95
André <andredekamacedo@gmail.com>
2023-08-12 22:10:00 -0300

    #128 - Add appsettings configuration files per environment

--- BugPorter.Client/MauiProgram.cs
+++ BugPorter.Client/MauiProgram.cs
@@ -14,6 +14,7 @@ public static class MauiProgram
             .UseMauiApp<App>()
             .UseMauiCommunityToolkit()
             .RegisterViewModelsViews()
+            .RegisterAppServicesAuth()
             .RegisterAppServices()
             .ConfigureFonts(fonts =>
             {
@@ -22,47 +23,14 @@ public static class MauiProgram
             });
         return builder.Build();
     }
-    public static void AddAppSettings(this MauiAppBuilder builder)
-    {
-        using Stream stream = Assembly
-            .GetExecutingAssembly()
-            .GetManifestResourceStream("BugPorter.Client.appsettings.json");
-        if(stream != null)
-        {
-            IConfigurationRoot config = new ConfigurationBuilder()
-                .AddJsonStream(stream)
-                .Build();
-            builder.Configuration.AddConfiguration(config);
-        }
 
-    }
     public static MauiAppBuilder RegisterAppServices(this MauiAppBuilder builder)
     {
         builder.Services.AddSingleton<CurrentUserStore>();
         builder.Services.AddSingleton<CurrentUserAuthHttpMessageHandler>();
-
-        string bugporterApiBaseUrl = builder.Configuration.GetValue<string>("BUGPORTER_API_BASE_URL");
-
-        builder.Services
-           .AddRefitClient<IReportBugApiCommand>()
-           .ConfigureHttpClient(c => c.BaseAddress = new Uri(bugporterApiBaseUrl))
-           .AddHttpMessageHandler<CurrentUserAuthHttpMessageHandler>();
-        
-        string firebaseApiKey = builder.Configuration.GetValue<string>("FIREBASE_API_KEY");
-        string firebaseAuthDomain = builder.Configuration.GetValue<string>("FIREBASE_AUTH_DOMAIN");
-
-        builder.Services.AddSingleton(new FirebaseAuthClient(new FirebaseAuthConfig()
-        {
-            ApiKey = firebaseApiKey,
-            AuthDomain = firebaseAuthDomain,
-            Providers = new FirebaseAuthProvider[]
-            {
-                new EmailProvider()
-            }
-        }));
-
         return builder;
     }
+
     public static MauiAppBuilder RegisterViewModelsViews(this MauiAppBuilder builder)
     {
         builder.Services.AddTransient<ReportBugViewModel>();
@@ -78,5 +46,57 @@ public static class MauiProgram
         builder.Services.AddTransient(s => new SignUpView(s.GetRequiredService<SignUpViewModel>()));
 
         return builder;
+    }
+    public static MauiAppBuilder RegisterAppServicesAuth(this MauiAppBuilder builder)
+    {
+        builder.AddAppSettings();
+
+        string bugporterApiBaseUrl = builder.Configuration.GetValue<string>("BUGPORTER_API_BASE_URL");
+        string firebaseApiKey = builder.Configuration.GetValue<string>("FIREBASE_API_KEY");
+        string firebaseAuthDomain = builder.Configuration.GetValue<string>("FIREBASE_AUTH_DOMAIN");
+
+        builder.Services
+           .AddRefitClient<IReportBugApiCommand>()
+           .ConfigureHttpClient(c => c.BaseAddress = new Uri(bugporterApiBaseUrl))
+           .AddHttpMessageHandler<CurrentUserAuthHttpMessageHandler>();
+
+        builder.Services.AddSingleton(new FirebaseAuthClient(new FirebaseAuthConfig()
+        {
+            ApiKey = firebaseApiKey,
+            AuthDomain = firebaseAuthDomain,
+            Providers = new FirebaseAuthProvider[]
+            {
+                new EmailProvider()
+            }
+        }));
+
+        return builder;
+    }
+    private static void AddAppSettings(this MauiAppBuilder builder)
+    {
+        builder.AddJsonSettings("appsettings.json");
+
+#if DEBUG
+        builder.AddJsonSettings("appsettings.development.json");
+#endif
+
+#if !DEBUG
+        builder.AddJsonSettings("appsettings.production.json");
+#endif
+    }
+
+    private static void AddJsonSettings(this MauiAppBuilder builder, string filename)
+    {
+        using Stream stream = Assembly
+            .GetExecutingAssembly()
+            .GetManifestResourceStream($"BugPorter.Client.{filename}");
+
+        if (stream != null)
+        {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .AddJsonStream(stream)
+                .Build();
+            builder.Configuration.AddConfiguration(config);
+        }
     }
 }
